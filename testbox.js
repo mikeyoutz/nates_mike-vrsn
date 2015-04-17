6a14db1453021b371257b9b1388fcd6c0b662eb4
mikeyoutz <ztuoy@comcast.net>
2015-04-16 19:54:39 -0600

    initial add to new nate repository on mike's git hub

---
+++ testbox.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Mike_HP-Lap on 3/31/2015.
+ */
+
+
+$(function(){
+
+    tabs.on('click', function(event){
+        tabs.removeClass('active');
+        panels.removeClass('active');
+}
